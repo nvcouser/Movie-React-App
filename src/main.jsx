e3c3369d165e4328c255a74ac7a20a633c6572d8
user <nvcouser@gmail.com>
2024-11-25 17:38:38 +0330

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -7,3 +8,15 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+>>>>>>> a9ba4c5 (start project)
